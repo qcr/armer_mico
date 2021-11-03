06f7f42fd4618e3c2e70750db3a29c8034b2d4cc
faith <limfw@qut.edu.au>
2021-11-03 12:06:02 +1000

    updated to work with latest rtb

--- armer_mico/models/Mico.py
+++ armer_mico/models/Mico.py
@@ -1,29 +1,22 @@
 #!/usr/bin/env python
 
 import numpy as np
-from pathlib import PurePosixPath
 from roboticstoolbox.robot.ERobot import ERobot
-from roboticstoolbox.tools import xacro
-from roboticstoolbox.tools import URDF
-from roboticstoolbox.tools import data
-from spatialmath import SE3
 from rospkg import RosPack
 
 class Mico(ERobot):
 
     def __init__(self):
-        old_base = data.base_path
-        data.set_base_path(RosPack().get_path('armer_mico') + '/data')
-        
-        #links, name, _, _ = self.URDF_read(
-        links, name = self.URDF_read(
-            "robots/m1n4s200_standalone.xacro"
-        )
 
-        data.set_base_path(old_base)
-                
+        links, name, urdf_string, urdf_filepath = self.URDF_read("robots/m1n4s200_standalone.xacro", tld=RosPack().get_path('armer_mico') + '/data/xacro')
+
         super().__init__(
-            links, name=name, manufacturer="Kinova", gripper_links=links[7]
+            links,
+            name=name,
+            urdf_string=urdf_string,
+            urdf_filepath=urdf_filepath,
+            manufacturer="Kinova", 
+            gripper_links=links[7]
         )
         
         self.addconfiguration(
@@ -31,6 +24,5 @@ class Mico(ERobot):
         )
 
 if __name__ == "__main__":  # pragma nocover
-
     robot = Mico()
     print(robot)
